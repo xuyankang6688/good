247ce95c145d88508f2d42463efebcdef46d0f11
xykang <ww.xykang@qq.vom>
2021-04-14 22:10:06 +0800

    test2

--- springBootTest/src/main/java/com/cn/fs/FsApplication.java
+++ springBootTest/src/main/java/com/cn/fs/FsApplication.java
@@ -9,7 +9,7 @@ public class FsApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(FsApplication.class, args);
-        System.err.println("==========="+"----------");
+        System.err.println("======dfsf====="+"----------");
         System.err.println("==========="+"fdgd----------");
     }
 
