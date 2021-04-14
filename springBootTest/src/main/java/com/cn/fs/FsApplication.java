a5cf2a9a29bdbad53fa5252968c348b718bf0859
xykang <ww.xykang@qq.vom>
2021-04-14 21:56:17 +0800

    test1

--- springBootTest/src/main/java/com/cn/fs/FsApplication.java
+++ springBootTest/src/main/java/com/cn/fs/FsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FsApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FsApplication.class, args);
+        System.err.println("===========");
     }
 
 }
